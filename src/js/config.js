a9f1e26d576ed33594c31ce28758efb24aa97626
Apekshit <apekshit33@gmail.com>
2023-11-20 12:53:53 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4b8364c9-96af-4dc8-81e7-ddba63a4f13e';
+export const MODAL_CLOSE_SEC = 2.5;
